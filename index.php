6a2391316d0dca0e84e8fbfccba4f2e07064987b
Lisa Susheel <lisasusheel12@gmail.com>
2023-05-04 23:12:02 +0530

    gg

---
+++ index.php
@@ -0,0 +1,33 @@
+<?php include 'connection.php';  ?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>
+
+    </title>
+</head>
+<body>
+    <div>
+<form ac action="" method="POST">
+
+<input type="text" name="firstname" placeholder="firstname" ><br><br>
+
+<input type="text" name="lastname" placeholder="lastname" ><br><br>
+
+<input type="number" name="age" placeholder="age" ><br><br>
+
+<input type="submit" name="save_btn" value="Save" ><br><br>
+
+
+
+
+</form>
+
+    </div>
+
+    <?php
+   
+    ?>
+</body>
+</html>
+
