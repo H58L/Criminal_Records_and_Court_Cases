84f420cbae2fb65fa416ba252521d03c83eba0db
kaushikikhabri <131288215+kaushikikhabri@users.noreply.github.com>
2023-05-06 22:25:26 +0530

    Update queries.php

--- queries.php
+++ queries.php
@@ -1,4 +1,6 @@
-<?php include 'connection.php';  ?>
+<?php
+include "conn.php";
+?>
 
 <!-- This is for user to execute qureies -->
 <!DOCTYPE html>
@@ -15,17 +17,110 @@
 <div class="container">
 <form action="" method="POST">
     <ol>
+        <div class="input field">
+<h3><li>View Cases and their details</h3></li>
+<input type="submit" name="query_4" value="View">
+
+
+
+        <?php 
+if(isset($_POST['query_4']))
+{
+    $sql=" SELECT * from cases;";
+    $result= mysqli_query($con,$sql);
+    
+    if (!$result) {
+        echo "FAILED";
+        die("Query failed: " . mysqli_error($con));
+        exit();
+    
+    }
+
+    echo "<table>";
+    echo "<tr><th>CaseID</th><th>CourtName</th><th>JudgeName</th><th>CaseType</th><th>HearingDate</th><th>Case_Status</th></tr>";
+    while ($row = mysqli_fetch_assoc($result)) {
+        echo "<tr><td>".$row['CaseID']."</td><td>".$row['CourtName']."</td><td>".$row['JudgeName']."</td><td>".$row['CaseType']."</td><td>".$row['HearingDate']."</td><td>".$row['Case_Status']."</td></tr>";
+    }
+    echo "</table>";
+}
+
+?>
+</div>
+
 <div class="input field">
+<h3><li>View Criminals and their details</h3></li>
+<input type="submit" name="query_5" value="View">
+
+
+
+        <?php 
+if(isset($_POST['query_5']))
+{
+    $sql=" SELECT * from criminal;";
+    $result= mysqli_query($con,$sql);
+    
+    if (!$result) {
+        echo "FAILED";
+        die("Query failed: " . mysqli_error($con));
+        exit();
+    
+    }
+
+    echo "<table>";
+    echo "<tr><th>CriminalID</th><th>CrimeID</th><th>FirstName</th><th>LastName</th><th>Gender</th><th>DOB</th><th>PhoneNumber</th><th>AadhaarNumber</th><th>State</th><th>City</th><th>Pincode</th></tr>";
+    while ($row = mysqli_fetch_assoc($result)) {
+        echo "<tr><td>".$row['CriminalID']."</td><td>".$row['CrimeID']."</td><td>".$row['FirstName']."</td><td>".$row['LastName']."</td><td>".$row['Gender']."</td><td>".$row['DOB']."</td><td>".$row['PhoneNumber']."</td><td>".$row['AadhaarNumber']."</td><td>".$row['States']."</td><td>".$row['City']."</td><td>".$row['Pincode']."</td></tr>";
+    }
+    echo "</table>";
+}
+
+?>
+</div>
+
+
+<div class="input field">
+<h3><li>View Victims and their details</h3></li>
+<input type="submit" name="query_6" value="View">
+
+
+
+        <?php 
+if(isset($_POST['query_6']))
+{
+    $sql=" SELECT * from victim;";
+    $result= mysqli_query($con,$sql);
+    
+    if (!$result) {
+        echo "FAILED";
+        die("Query failed: " . mysqli_error($con));
+        exit();
+    
+    }
+
+    echo "<table>";
+    echo "<tr><th>VictimID</th><th>CrimeID</th><th>FirstName</th><th>LastName</th><th>Gender</th><th>DOB</th><th>PhoneNumber</th><th>AadhaarNumber</th><th>State</th><th>City</th><th>Pincode</th></tr>";
+    while ($row = mysqli_fetch_assoc($result)) {
+        echo "<tr><td>".$row['VictimID']."</td><td>".$row['CrimeID']."</td><td>".$row['firstname']."</td><td>".$row['lastname']."</td><td>".$row['Gender']."</td><td>".$row['DOB']."</td><td>".$row['PhoneNo']."</td><td>".$row['AadhaarNumber']."</td><td>".$row['States']."</td><td>".$row['City']."</td><td>".$row['Pincode']."</td></tr>";
+    }
+    echo "</table>";
+}
+
+?>
+</div>
+
+
+<div class="input field">
+
 <h3><li>View number of crimes per state</li></h3>
 <input type="submit" name="query_1" value="View">
 
 <?php 
 if(isset($_POST['query_1']))
 {
-    $sql="SELECT State, COUNT(*) as num_crimes
+    $sql="SELECT States, COUNT(*) as num_crimes
     FROM crime
-    GROUP BY State
-    ORDER BY num_crimes  DESC
+    GROUP BY States
+    ORDER BY num_crimes DESC
     ";
     $result= mysqli_query($con,$sql);
     
@@ -44,7 +139,7 @@ if(isset($_POST['query_1']))
     echo "<table>";
     echo "<tr><th>State</th><th>Number of Crimes</th></tr>";
     while ($row = mysqli_fetch_assoc($result)) {
-        echo "<tr><td>".$row['State']."</td><td>".$row['num_crimes']."</td></tr>";
+        echo "<tr><td>".$row['States']."</td><td>".$row['num_crimes']."</td></tr>";
     }
     echo "</table>";
     
@@ -53,6 +148,10 @@ if(isset($_POST['query_1']))
 ?>
 
 </div>
+
+
+
+
 <div class="input field">
     <li> <h3>View State with maximum crimes</li></h3>
     <input type="submit" name="query_2" value="View">
@@ -60,9 +159,9 @@ if(isset($_POST['query_1']))
     <?php 
 if(isset($_POST['query_2']))
 {
-    $sql="SELECT State, COUNT(*) as num_crimes
+    $sql="SELECT States, COUNT(*) as num_crimes
     FROM crime
-    GROUP BY State
+    GROUP BY States
     ORDER BY num_crimes  DESC LIMIT 1
     ";
     $result= mysqli_query($con,$sql);
@@ -82,7 +181,7 @@ if(isset($_POST['query_2']))
     echo "<table>";
     echo "<tr><th>State</th><th>Crimes</th></tr>";
     while ($row = mysqli_fetch_assoc($result)) {
-        echo "<tr><td>".$row['State']."</td><td>".$row['num_crimes']."</td></tr>";
+        echo "<tr><td>".$row['States']."</td><td>".$row['num_crimes']."</td></tr>";
     }
     echo "</table>";
     
@@ -99,7 +198,7 @@ if(isset($_POST['query_2']))
 <?php 
 if(isset($_POST['query_3']))
 {
-    $sql="SELECT YEAR(Date) AS year, COUNT(*) AS num_crimes FROM crime GROUP BY year ORDER BY num_crimes DESC
+    $sql="SELECT YEAR(Crime_Date) AS year, COUNT(*) AS num_crimes FROM crime GROUP BY year ORDER BY num_crimes DESC
     ";
     $result= mysqli_query($con,$sql);
     
@@ -128,13 +227,53 @@ if(isset($_POST['query_3']))
 
 
 </div>
+<div class="input field">
+<li><h3>View number of crimes city wise</h3></li>
+<input type="submit" name="query_5" value="View">
 
+<?php 
+if(isset($_POST['query_5']))
+{
+    $sql="SELECT City, COUNT(*) as num_crimes
+    FROM crime
+    GROUP BY City
+    ORDER BY num_crimes  DESC
+    ";
+    $result= mysqli_query($con,$sql);
+    
+    if (!$result) {
+        echo "FAILED";
+        die("Query failed: " . mysqli_error($con));
+        exit();
+    
+    }
+    // else{
+    //     header("Location: queries.php");
+    //      exit();
+    // }
+    
+    // Display the results
+    echo "<table>";
+    echo "<tr><th>City</th><th>Number of Crimes</th></tr>";
+    while ($row = mysqli_fetch_assoc($result)) {
+        echo "<tr><td>".$row['City']."</td><td>".$row['num_crimes']."</td></tr>";
+    }
+    echo "</table>";
+    
+}
 
-
-</ol>
-</form>
-
+?>
 
 </div>
+<div class="input field">
+
+<li><h3>View City with maximum number of crimes</h3></li>
+
+</div>
+</ol>
+</form>
+</div>
+
 </body>
 </html>
+
